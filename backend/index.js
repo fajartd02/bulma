e2ad3d387ac8dceb63f1589714dfea3510155e11
unknown <fajarmh2@gmail.com>
2022-04-13 22:51:07 +0700

    Create getUser function controller

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "./config/Database.js";
-import Users from "./models/UserModel.js";
+// import Users from "./models/UserModel.js"; // Not using again cuz i've create it
 
 const app = express();
 const PORT = 5000;
@@ -8,7 +8,7 @@ const PORT = 5000;
 try {
     await db.authenticate();
     console.log("Database Connected...");
-    await Users.sync(); // If not have table, auto create generate.
+    // await Users.sync(); // If not have table, auto create generate.
 } catch(err) {
     console.log(err);
 }
