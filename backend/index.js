a4ee91260aff9cd47cceaa138190404d1374bd91
unknown <fajarmh2@gmail.com>
2022-04-13 22:40:28 +0700

    Setting up config db mysql with sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import db from "./config/Database.js";
+
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => res.send("Test"));
+try {
+    await db.authenticate();
+    console.log("Database Connected...");
+} catch(err) {
+    console.log(err);
+}
 
 app.listen(PORT, () => console.log(`server running at port http://localhost:${PORT}/`));
