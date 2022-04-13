d0f10dae6042ea88ec9f21c1b0646343b33ce017
unknown <fajarmh2@gmail.com>
2022-04-13 22:56:52 +0700

    Create controler and routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/Database.js";
+import router from "./routes/index.js";
 // import Users from "./models/UserModel.js"; // Not using again cuz i've create it
 
 const app = express();
@@ -12,5 +13,9 @@ try {
 } catch(err) {
     console.log(err);
 }
+
+// Middleware
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`server running at port http://localhost:${PORT}/`));
