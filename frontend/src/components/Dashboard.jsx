6aa8ca9116b557aff817c8f4ec361c16586ec58d
unknown <fajarmh2@gmail.com>
2022-04-15 16:07:27 +0700

    Create get Users

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/token');
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+
+      setName(decoded.name);
+      setExpire(decoded.exp);
+
+    } catch(err) {
+      if(err.response) {
+          navigate('/');
+      }
+    }
+  }
+
+  const getUsers = async() => {
+    const response = await axios.get('http://localhost:5000/users', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    console.log(response.data);
+  }
+
   return (
     <div className='container mt-5'>
-        <h1>Welcome Back: </h1>
+        <h1>Welcome Back: {name}</h1>
+        <button onClick={getUsers} className='button is-info'>Get Users</button>
     </div>
   )
 }
