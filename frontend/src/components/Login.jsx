e3e1f68d22bfb64197c8f26e4e70d880f3ddf117
unknown <fajarmh2@gmail.com>
2022-04-15 15:49:43 +0700

    Create logic ui auth page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,31 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const navigate = useNavigate();
+
+  const Auth = async (e) => {
+    e.preventDefault(); // agar page tidak reload
+    try {
+      await axios.post('http://localhost:5000/login', {
+        email: email,
+        password: password,
+      });
+
+      navigate("/dashboard");
+    } catch (err) {
+      if (err.response) {
+        setMsg(err.response.data.message);
+      }
+    }
+  }
   return (
     <section className="hero has-background-grey-light is-fullheight is-fullwidth">
       <div className="hero-body">
         <div className="container">
-            <div className="columns is-centered">
-                <div className="columns is-4-desktop">
-                    <form className="box">
-                      <div className="field mt-2">
-                        <label className='label'>Email or Username</label>
-                        <div className="controls">
-                          <input type="text" className='input' placeholder='Username' />
-                        </div>
-                      </div>
-                      <div className="field mt-5">
-                        <label className='label'>Password</label>
-                        <div className="controls">
-                          <input type="password" className='input' placeholder='******' />
-                        </div>
-                      </div>
-                      <div className="field mt-5">
-                        <button className='button is-success is-fullwidth'>Login</button>
-                      </div>
-                    </form>
+          <div className="columns is-centered">
+            <div className="columns is-4-desktop">
+              <form className="box" onSubmit={Auth}>
+                <p className='has-text-centered has-text-danger'>{msg}</p>
+                <div className="field mt-2">
+                  <label className='label'>Email or Username</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className='input'
+                      placeholder='Username'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
                 </div>
+                <div className="field mt-5">
+                  <label className='label'>Password</label>
+                  <div className="controls">
+                    <input type="password"
+                      className='input'
+                      placeholder='******'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className='button is-success is-fullwidth'>Login</button>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
       </div>
     </section>
